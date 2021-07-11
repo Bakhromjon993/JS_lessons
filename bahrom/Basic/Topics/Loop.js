164bb37d4a3006b6c5f820ee5b878389064cb5d6
bakhromjon993 <baxrom.yuldoshev@gmail.com>
2021-07-11 15:54:04 +0500

    first

---
+++ bahrom/Basic/Topics/Loop.js
@@ -0,0 +1,24 @@
+let text = ''
+// for (let i = 10; i > 5; i--) {
+//     text += `This text is coming ${i} times`
+// }
+// console.log(text)
+
+// let i = 5;
+// for (i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+// console.log(text)
+
+let nums = [5,6,3,6,2,6,8,32,3234,534,34,64]
+// for (const i in nums) {
+//     if (nums[i] > 2000){
+//         text = nums[i]
+//     }
+// }
+// console.log(text)
+
+nums.forEach((value, index, array) => {
+    console.log(text = array)
+})
+
